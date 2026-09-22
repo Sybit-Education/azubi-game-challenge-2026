e149c5632e76eff6a01182f93ff46f657dc72cb6
hansolo <hsn@sybit.de>
2026-09-22 14:06:21 +0200

    First Mockup

--- src/main.js
+++ src/main.js
@@ -1,105 +1,64 @@
-class Car extends Phaser.GameObjects.Image {
-  constructor(scene, x, y) {
-    super(scene, x, y, `car`);
-    scene.add.existing(this);
-    this.value = 10;
-    this.setScale(0.25);
-    this.setAngle(180);
-  }
-  move() {
-    this.scene.input.keyboard.on('keydown-A', () => {
-      this.x -= 5;
-    });
-    this.scene.input.keyboard.on('keydown-D', () => {
-      this.x += 5;
-    });
-  }
-}
+class Car extends Phaser.GameObjects.Image{
+    constructor(scene, x, y) {
+        super(scene, x, y,"car");
+        scene.add.existing(this);
+        this.value = 10
+        this.setScale(0.5);
 
-function show_main_menu(scene) {
-  const button = scene.add
-    .text(400, 300, 'Start Game', {
-      fontSize: '32px',
-      backgroundColor: '#00000',
-      padding: { x: 10, y: 5 },
-    })
-    .setOrigin(0.5)
-    .setInteractive();
-
-  button.on('pointerdown', () => {
-    button.setVisible(false);
-    const car = new Car(scene, scene.scale.width / 2, scene.scale.height / 1.25);
-
-    car.move();
-
-    scene.add
-      .text(scene.scale.width - 20, 20, `Score: ${car.value}`, {
-        fontSize: '30px',
-      })
-      .setOrigin(1, 0);
-  });
-
-  this.scene.input.keyboard.on('keydown-D', () => {
-    if (this.lane < lanes.length - 1) {
-      this.lane++;
-      this.x = lanes[this.lane];
     }
-  });
+ 
+    move(){
+        this.scene.input.keyboard.on('keydown-A', () => {
+            this.x -= 20
+        });
+        this.scene.input.keyboard.on('keydown-D', () => {
+            this.x += 20
+        });  
+    }
 }
-
-class Obstacle extends Phaser.Physics.Arcade.Sprite {
-  constructor(scene, x, y) {
-    super(scene, x, y, 'obstacle-car');
-    scene.add.existing(this);
-    scene.physics.add.existing(this);
-    this.setScale(0.3);
-  }
+ 
+function show_main_menu(scene){
+    const button = scene.add.text(400, 300, "Start Game", {
+        fontSize: "32px",
+        backgroundColor: "#00000",
+        padding: { x: 10, y: 5 }
+    }).setOrigin(0.5).setInteractive();
+ 
+    button.on("pointerdown", () => {
+        button.setVisible(false);
+        const car = new Car(scene, scene.scale.width / 2, scene.scale.height / 1.25, 75, 120, 0xffffff);
+        car.move();
+       
+        scene.add.text(scene.scale.width - 20, 20, `Score: ${car.value}`, {
+            fontSize: "30px"
+        }).setOrigin(1,0);
+    });
 }
-
+ 
+let width = window.innerWidth;
+let height = width * 9 / 16;
+if (height > window.innerHeight) {
+    height = window.innerHeight;
+    width = height * 16 / 9;
+}
+ 
 const config = {
-  type: Phaser.AUTO,
-  scale: {
-    mode: Phaser.Scale.NONE,
-    width: 800,
-    height: 600,
-  },
-  backgroundColor: '#fffff',
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 0 },
-      debug: false,
+    type: Phaser.AUTO,
+    scale: {
+        mode: Phaser.Scale.RESIZE,
+        width: window.innerWidth,
+        height: window.innerHeight
     },
-  },
-  scene: {
-    preload() {
-      this.load.image('obstacle-car', '../../sprites/obstacleCar.png');
-    },
-
-    create() {
-      const car = new Car(this, lanes[1], 500, 150, 75, 0xffffff);
-      car.move();
-
-      this.add
-        .text(this.scale.width - 20, 20, `Score: ${car.value}`, {
-          fontSize: '30px',
-        })
-        .setOrigin(1, 0);
-
-      const spawnObstacle = () => {
-        const obstacle = new Obstacle(this, Phaser.Math.RND.pick(lanes), 50);
-
-        obstacle.body.setVelocityY(200);
-      };
-
-      this.time.addEvent({
-        delay: 2000,
-        callback: spawnObstacle,
-        callbackScope: this,
-        loop: true,
-      });
-    },
-  },
+    backgroundColor: '#000000',
+    scene: {
+      preload(){
+        this.load.image(`car`,`sprites/race_car.png`);
+      },
+      create() {
+          show_main_menu(this);
+      }
+       
+    }
 };
-
+ 
 new Phaser.Game(config);
