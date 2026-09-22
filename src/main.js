122b9e338e8422888a0a1e366d62beaf9c29b851
hansolo <hsn@sybit.de>
2026-09-22 13:25:02 +0200

    test

--- src/main.js
+++ src/main.js
@@ -1,50 +1,51 @@
-
-class Car extends Phaser.GameObjects.Image{
-    constructor(scene, x, y) {
-        super(scene, x, y,`car`);
-        scene.add.existing(this);
-        this.value = 10
-        this.setScale(0.25);
-        this.setAngle(180);
-    }
-    move(){
-        this.scene.input.keyboard.on('keydown-A', () => {
-            this.x -= 5
-        });
-        this.scene.input.keyboard.on('keydown-D', () => {
-            this.x += 5
-        });  
-    }
-}
-
-
-function show_main_menu(scene){
-    const button = scene.add.text(400, 300, "Start Game", {
-        
-        fontSize: "32px",
-        backgroundColor: "#00000",
-        padding: { x: 10, y: 5 }
-    }).setOrigin(0.5).setInteractive();
-
-    button.on("pointerdown", () => {
-        button.setVisible(false);
-        const car = new Car(scene, scene.scale.width / 2, scene.scale.height / 1.25);
-
-        car.move();
-        
-        scene.add.text(scene.scale.width - 20, 20, `Score: ${car.value}`, {
-            fontSize: "30px"
-        }).setOrigin(1,0);
+class Car extends Phaser.GameObjects.Image {
+  constructor(scene, x, y) {
+    super(scene, x, y, `car`);
+    scene.add.existing(this);
+    this.value = 10;
+    this.setScale(0.25);
+    this.setAngle(180);
+  }
+  move() {
+    this.scene.input.keyboard.on('keydown-A', () => {
+      this.x -= 5;
     });
-
     this.scene.input.keyboard.on('keydown-D', () => {
-      if (this.lane < lanes.length - 1) {
-        this.lane++;
-        this.x = lanes[this.lane];
-      }
+      this.x += 5;
     });
   }
+}
 
+function show_main_menu(scene) {
+  const button = scene.add
+    .text(400, 300, 'Start Game', {
+      fontSize: '32px',
+      backgroundColor: '#00000',
+      padding: { x: 10, y: 5 },
+    })
+    .setOrigin(0.5)
+    .setInteractive();
+
+  button.on('pointerdown', () => {
+    button.setVisible(false);
+    const car = new Car(scene, scene.scale.width / 2, scene.scale.height / 1.25);
+
+    car.move();
+
+    scene.add
+      .text(scene.scale.width - 20, 20, `Score: ${car.value}`, {
+        fontSize: '30px',
+      })
+      .setOrigin(1, 0);
+  });
+
+  this.scene.input.keyboard.on('keydown-D', () => {
+    if (this.lane < lanes.length - 1) {
+      this.lane++;
+      this.x = lanes[this.lane];
+    }
+  });
+}
 
 class Obstacle extends Phaser.Physics.Arcade.Sprite {
   constructor(scene, x, y) {
